1e79b1314b6f81b57c97c9d317269a7493f2aff5
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2021-09-07 10:20:10 +0200

    Added regions and regions loading process

--- egon/map/models.py
+++ egon/map/models.py
@@ -1,23 +1,62 @@
+
 from django.contrib.gis.db import models
-from .managers import RegionMVTManager, LabelMVTManager, MVTManager, CenterMVTManager
+from django.utils.translation import gettext_lazy as _
+
+from .managers import MVTManager, RegionMVTManager, LabelMVTManager
 
 
 # REGIONS
 
-
 class Region(models.Model):
+    """Base class for all regions - works as connector to other models"""
+
+    class LayerType(models.TextChoices):
+        COUNTRY = "country", _("Land")
+        STATE = "state", _("Bundesland")
+        DISTRICT = "district", _("Kreis")
+        MUNICIPALITY = "municipality", _("Gemeinde")
+
+    layer_type = models.CharField(max_length=12, choices=LayerType.choices, null=False)
+
+
+class Country(models.Model):
     geom = models.MultiPolygonField(srid=4326)
     name = models.CharField(max_length=50, unique=True)
+
+    region = models.OneToOneField("Region", on_delete=models.DO_NOTHING, null=True)
 
     objects = models.Manager()
     vector_tiles = RegionMVTManager(columns=["id", "name", "bbox"])
     label_tiles = LabelMVTManager(geo_col="geom_label", columns=["id", "name"])
 
-    data_file = "Gha_AdminBoundaries"
-    layer = "Gha_Regions_01"
+    data_folder = "0_Boundaries"
+    data_file = "egon_boundaries_country"
+    layer = "egon_boundaries_country"
     mapping = {
         "geom": "MULTIPOLYGON",
-        "name": "Region",
+        "name": "gen",
+    }
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    geom = models.MultiPolygonField(srid=4326)
+    name = models.CharField(max_length=50, unique=True)
+
+    region = models.OneToOneField("Region", on_delete=models.DO_NOTHING, null=True)
+
+    objects = models.Manager()
+    vector_tiles = RegionMVTManager(columns=["id", "name", "bbox"])
+    label_tiles = LabelMVTManager(geo_col="geom_label", columns=["id", "name"])
+
+    data_folder = "0_Boundaries"
+    data_file = "egon_boundaries_state"
+    layer = "egon_boundaries_state"
+    mapping = {
+        "geom": "MULTISURFACE",
+        "name": "gen",
     }
 
     def __str__(self):
@@ -27,23 +66,41 @@ class Region(models.Model):
 class District(models.Model):
     geom = models.MultiPolygonField(srid=4326)
     name = models.CharField(max_length=50, unique=True)
-    area = models.FloatField()
-    population = models.BigIntegerField()
 
-    region = models.ForeignKey("Region", on_delete=models.CASCADE, related_name="districts")
+    region = models.OneToOneField("Region", on_delete=models.DO_NOTHING, null=True)
 
     objects = models.Manager()
     vector_tiles = RegionMVTManager(columns=["id", "name", "bbox"])
     label_tiles = LabelMVTManager(geo_col="geom_label", columns=["id", "name"])
 
-    data_file = "Gha_AdminBoundaries"
-    layer = "Gha_Districts_02"
+    data_folder = "0_Boundaries"
+    data_file = "egon_boundaries_district"
+    layer = "egon_boundaries_district"
     mapping = {
-        "geom": "MULTIPOLYGON",
-        "name": "District",
-        "area": "Area_km2",
-        "population": "Pop2020",
-        "region": {"name": "Region"},  # ForeignKey see https://stackoverflow.com/a/46689928/5804947
+        "geom": "MULTISURFACE",
+        "name": "gen",
+    }
+
+    def __str__(self):
+        return self.name
+
+
+class Municipality(models.Model):
+    geom = models.MultiPolygonField(srid=4326)
+    name = models.CharField(max_length=50, unique=True)
+
+    region = models.OneToOneField("Region", on_delete=models.DO_NOTHING, null=True)
+
+    objects = models.Manager()
+    vector_tiles = RegionMVTManager(columns=["id", "name", "bbox"])
+    label_tiles = LabelMVTManager(geo_col="geom_label", columns=["id", "name"])
+
+    data_folder = "0_Boundaries"
+    data_file = "egon_boundaries_municipality"
+    layer = "egon_boundaries_municipality"
+    mapping = {
+        "geom": "MULTISURFACE",
+        "name": "gen",
     }
 
     def __str__(self):
--- egon/utils/data_processing.py
+++ egon/utils/data_processing.py
@@ -2,26 +2,51 @@ import os
 
 from config.settings.base import ROOT_DIR
 from egon.utils.ogr_layer_mapping import RelatedModelLayerMapping
-from egon.map.models import Region, District, Hospitals, HospitalsSimulated, Cluster, Nightlight
+from egon.map.models import Region, Country, State, District, Municipality
 
 
-MODELS = [
-    Region,
+REGIONS = [
+    Country,
+    State,
     District,
-    Hospitals,
-    HospitalsSimulated,
-    Cluster,
-    Nightlight,
+    Municipality,
 ]
+
+MODELS = []
+
+
+def load_regions(regions=None, verbose=True):
+    regions = regions or REGIONS
+    for region in regions:
+        print(f"Upload data for region '{region.__name__}'")
+        if hasattr(region, "data_folder"):
+            data_path = os.path.join(ROOT_DIR, "egon", "data", region.data_folder, f"{region.data_file}.gpkg")
+        else:
+            data_path = os.path.join(ROOT_DIR, "egon", "data", f"{region.data_file}.gpkg")
+        region_model = Region(layer_type=region.__name__.lower())
+        region_model.save()
+        instance = RelatedModelLayerMapping(
+            model=region,
+            data=data_path,
+            mapping=region.mapping,
+            layer=region.layer,
+            transform=4326,
+        )
+        instance.region = region_model
+        instance.save(strict=True, verbose=verbose)
 
 
 def load_data(models=None, verbose=True):
     models = models or MODELS
     for model in models:
         print(f"Upload data for model '{model.__name__}'")
+        if hasattr(model, "data_folder"):
+            data_path = os.path.join(ROOT_DIR, "egon", "data", model.data_folder, f"{model.data_file}.gpkg")
+        else:
+            data_path = os.path.join(ROOT_DIR, "egon", "data", f"{model.data_file}.gpkg")
         instance = RelatedModelLayerMapping(
             model=model,
-            data=os.path.join(ROOT_DIR, "egon", "data", f"{model.data_file}.gpkg",),
+            data=data_path,
             mapping=model.mapping,
             layer=model.layer,
             transform=4326,
