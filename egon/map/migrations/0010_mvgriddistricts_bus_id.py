04aded2edb3c6f716bf68b1ac33feeedc9e7614a
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-05-05 11:04:38 +0200

    Fix not null error for existing mvgriddistricts

--- egon/map/migrations/0010_mvgriddistricts_bus_id.py
+++ egon/map/migrations/0010_mvgriddistricts_bus_id.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mvgriddistricts',
             name='bus_id',
-            field=models.IntegerField(default=None),
+            field=models.IntegerField(default=None, null=True),
             preserve_default=False,
         ),
     ]
