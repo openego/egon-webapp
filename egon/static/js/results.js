cb2321b9230c99397ffd253e09943a155526ce95
finnus <40977905+finnus@users.noreply.github.com>
2023-05-16 15:26:27 +0200

    re-enable disabling of choropleths

--- egon/static/js/results.js
+++ egon/static/js/results.js
@@ -5,12 +5,22 @@ const layerSwitches = document.querySelectorAll(".form-check-input");
 layerSwitches.forEach(layerSwitch => {
   layerSwitch.addEventListener("change", function() {
     PubSub.publish(mapEvent.CHOROPLETH_SELECTED, layerSwitch.value);
-    layerSwitches.forEach(otherSwitch => {
+    if (this.checked) {
+      layerSwitches.forEach(otherSwitch => {
         if (otherSwitch.id !== layerSwitch.id && otherSwitch.dataset.geomLayer === layerSwitch.dataset.geomLayer) {
           otherSwitch.checked = false;
         }
       });
       map.setLayoutProperty(layerSwitch.dataset.geomLayer, "visibility", "visible");
       activateChoropleth(null, layerSwitch.id);
+    } else {
+      map.setLayoutProperty(layerSwitch.dataset.geomLayer, "visibility", "none");
+        if (layerSwitch.id !== layerSwitch.dataset.geomLayer) {
+          deactivateChoropleth(null, layerSwitch.id);
+        }
+        if (layerSwitch.id === "mv_grid_district_data" ) {
+          deactivateChoropleth(null, layerSwitch.id);
+        }
+    }
   });
 });
