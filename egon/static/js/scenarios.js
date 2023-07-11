fcba97534d1d34951d66e8b80b1eb548634b0597
finnus <40977905+finnus@users.noreply.github.com>
2023-07-11 09:33:26 +0200

    add scenario description

--- egon/static/js/scenarios.js
+++ egon/static/js/scenarios.js
@@ -1,18 +1,24 @@
 // Get references to the buttons
 var button2035 = document.getElementById("2035");
 var button100RE = document.getElementById("100RE");
+var description_2035 = document.getElementById("scenario-description-egon2035");
+var description_100RE = document.getElementById("scenario-description-100RE");
 
 // Add click event listeners
 button2035.addEventListener("click", function () {
     button100RE.classList.remove("active");
     button2035.classList.add("active");
     toggleLayers("2035"); // Show layers for 2035 scenario
+    description_100RE.style.display = "none";
+    description_2035.style.display = "";
 });
 
 button100RE.addEventListener("click", function () {
     button2035.classList.remove("active");
     button100RE.classList.add("active");
     toggleLayers("100RE"); // Show layers for 100RE scenario
+    description_2035.style.display = "none";
+    description_100RE.style.display = "";
 });
 
 function toggleLayers(scenario) {
