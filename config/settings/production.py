ba15ee08668530302437b00d13bab4cd30e736d1
finnus <40977905+finnus@users.noreply.github.com>
2024-01-31 09:49:46 +0100

    easier dockerfile

--- config/settings/production.py
+++ config/settings/production.py
@@ -112,7 +112,7 @@ LOGGING = {
     "formatters": {"verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"}},
     "handlers": {
         "console": {
-            "level": "ERROR",
+            "level": "DEBUG",
             "class": "logging.StreamHandler",
             "formatter": "verbose",
         }
